55d40dfbbcf95eab8f360be0894cba6452e70ae3
emileyray <69918609+emileyray@users.noreply.github.com>
2021-09-28 00:11:02 +0300

    solved errors from merging

--- lib/widgets/large_screen.dart
+++ lib/widgets/large_screen.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide User;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
