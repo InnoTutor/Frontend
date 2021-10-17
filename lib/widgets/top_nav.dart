237fa3b5b07c897c5d834b8cd2f3c5b180454e32
emileyray <69918609+emileyray@users.noreply.github.com>
2021-10-17 17:32:28 +0300

    Update top_nav.dart

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -1,15 +1,12 @@
 import 'dart:ui';
 import 'package:flutter/cupertino.dart' hide Color;
 import 'package:flutter/material.dart'hide Color;
-import 'package:inno_tutor/models/user.dart';
-import 'package:inno_tutor/pages/login_page.dart';
 import 'package:inno_tutor/services/auth.dart';
 import '../constants/style.dart' as style;
-import '../helpers/responsiveness.dart'hide Color;
-import 'package:inno_tutor/globals.dart' as globals hide Color;
-import '../layout.dart';
-import 'custom_text.dart'hide Color;
-import 'logo.dart'hide Color;
+import '../helpers/responsiveness.dart';
+import 'package:inno_tutor/globals.dart' as globals;
+import 'custom_text.dart';
+import 'logo.dart';
 
 class myAppBar extends StatefulWidget implements PreferredSizeWidget {
   final Function() notifyParent;
@@ -27,83 +24,79 @@ class _myAppBarState extends State<myAppBar> {
   Widget build(BuildContext context) {
     return AppBar(
       leading: ResponsiveWidget.isSmallScreen(context)
-          ? IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                // Scaffold.of(context).openDrawer();
-              },
-            )
-          : null,
+        ? IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            // Scaffold.of(context).openDrawer();
+          },
+        )
+        : null,
       automaticallyImplyLeading: false,
       elevation: 0,
       backgroundColor: style.darkGreen,
       title: Row(
         children: [
           !ResponsiveWidget.isSmallScreen(context)
-              ? Logo(height: 30, leftPadding: 20, rightPadding: 0)
-              : Container(),
-          // Expanded(child: Container()),
-          Container(
-            width: 15,
-          ),
-          Expanded(
-            child: Container(),
-          ),
+            ? Logo(height: 30, leftPadding: 20, rightPadding: 0)
+            : Container(),
+          Container(width: 15),
+          Expanded(child: Container()),
           Container(
             child: ResponsiveWidget.isSmallScreen(context) || globals.user == null
-                ? null
-                : DropdownButtonHideUnderline(
-                  child: DropdownButton(
-                      value: _value,
-                      items: <DropdownMenuItem<int>>[
-                        new DropdownMenuItem(
-                          child: !ResponsiveWidget.isSmallScreen(context)
-                              ? Row(children: [
-                                  CustomText(
-                                    text: globals.user.name + " " + globals.user.surname,
-                                    size: 18,
-                                    color: Colors.black,
-                                    weight: FontWeight.w600,
-                                  ),
-                                  Container(
-                                      padding: EdgeInsets.all(2),
-                                      child:CircleAvatar(
-                                              radius: 18,
-                                              backgroundColor: style.lightGreen,
-                                              child: CircleAvatar(
-                                                  radius: 30,
-                                                  backgroundImage: NetworkImage(globals.user.picture)))),
-                                ])
-                              : Container(),
-                          value: 0,
-                        ),
-                        new DropdownMenuItem(
-                          child: new Text('Logout'),
-                          value: 1,
-                          onTap: ()async{
-                            await new AuthService().signOutGoogle();
-                            setState(() {
-                            _value=0;  
-                            });
-                            widget.logOut();
-
-                          },
-                        ),
-                      ],
-                      onChanged: (value) {
+              ? null
+              : DropdownButtonHideUnderline(
+                child: DropdownButton(
+                  value: _value,
+                  items: <DropdownMenuItem<int>>[
+                    new DropdownMenuItem(
+                      child: !ResponsiveWidget.isSmallScreen(context)
+                        ? Row(children: [
+                          CustomText(
+                            text: globals.user.name + " " + globals.user.surname,
+                            size: 18,
+                            color: Colors.black,
+                            weight: FontWeight.w600,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(2),
+                            child:CircleAvatar(
+                              radius: 18,
+                              backgroundColor: style.lightGreen,
+                              child: CircleAvatar(
+                                radius: 30,
+                                backgroundImage: NetworkImage(globals.user.picture)
+                              )
+                            )
+                          ),
+                        ]) : Container(),
+                      value: 0,
+                    ),
+                    new DropdownMenuItem(
+                      child: new Text('Logout'),
+                      value: 1,
+                      onTap: ()async{
+                        await new AuthService().signOutGoogle();
                         setState(() {
-                          _value = value;
+                        _value=0;  
                         });
+                        widget.logOut();
                       },
                     ),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value;
+                    });
+                  },
                 ),
+              ),
           ),
           Container(
             width: 20,
           ),
           ResponsiveWidget.isSmallScreen(context)
-              ? Logo(height: 30, leftPadding: 0, rightPadding: 0)
-              : Container(),
+            ? Logo(height: 30, leftPadding: 0, rightPadding: 0)
+            : Container(),
         ],
       ),
       iconTheme: IconThemeData(color: style.lightGrey),
